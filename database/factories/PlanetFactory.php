367ba9a5712f0221466e8f76d78ab1c0cc367f95
albertofelipe <felipe0032sena@gmail.com>
2025-04-11 17:47:08 -0300

    feat: add Planet seeder and factory

--- database/factories/PlanetFactory.php
+++ database/factories/PlanetFactory.php
@@ -4,20 +4,15 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Planet>
- */
 class PlanetFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
+    
     public function definition(): array
     {
         return [
-            //
+            'name' => $this->faker->name(),
+            'climate' => $this->faker->randomElement(['desert', 'frozen', 'temperate']),
+            'terrain' => $this->faker->randomElement(['arid', 'snowy', 'temperate'])
         ];
     }
 }
--- database/seeders/PlanetSeeder.php
+++ database/seeders/PlanetSeeder.php
@@ -4,14 +4,15 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Planet;
+
 
 class PlanetSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        Planet::factory()
+            ->count(20)
+            ->create();
     }
 }
